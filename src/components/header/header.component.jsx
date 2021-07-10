72ec847215503fc66bcf8c522a059907daaa4ff2
Anand Raja <meetanandns@gmail.com>
2021-07-10 18:30:00 +0000

    user reselect logic - implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,8 @@ import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectHidden } from '../../redux/cart/cart.selectors';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -35,9 +37,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: {currentUser}, cart: {hidden} }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = state => ({
+  currentUser: selectCurrentUser(state),
+  hidden: selectHidden(state)
 });
 
 export default connect(mapStateToProps)(Header);
