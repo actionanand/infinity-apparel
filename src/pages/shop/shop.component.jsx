c33840c51429581925b431be1b3d4be767f80f87
Anand Raja <meetanandns@gmail.com>
2021-07-12 19:13:40 +0000

    refactoring - moved shop page content inside collections overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import './shop.styles.scss';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections }) => (
-<div className='shop-page'>
-    {
-      collections.filter((item, index) => index < 4)
-      .map(({id, ...otherCollectionProps}) => (
-        <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-      ))
-    }
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
   </div>
 );
   
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
