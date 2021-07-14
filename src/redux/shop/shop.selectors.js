8ead821f04776feb94ef5a63cea08d159638eb3e
Anand Raja <meetanandns@gmail.com>
2021-07-14 15:28:37 +0000

    converting obj into array

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,12 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+// converting obj into array
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 //curried function
 export const selectCollection = memoize((collectionUrlParam) => createSelector(
   [selectCollections],
