a2facb1e6ae9b2cb51a380be978a7fdbd7daa6f8
Anand Raja <meetanandns@gmail.com>
2021-07-31 06:03:07 +0000

    refactoring - saga logic

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 
@@ -20,8 +18,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-
-    const { setCurrentUser } = this.props;
 
     // auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -70,8 +66,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchedToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchedToProps)(App);
+export default connect(mapStateToProps)(App);
