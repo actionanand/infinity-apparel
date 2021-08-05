35b53a49a651e18096b7a382c2cd239cb017322d
Anand Raja <meetanandns@gmail.com>
2021-08-05 09:49:09 +0000

    useContext hook

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import './collection.styles.scss';
 
@@ -6,27 +6,21 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import CollectionsContext from '../../contexts/collections/collections.context';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  
   return (
-    <CollectionsContext.Consumer>
-      {
-        collections => {
-          const collection = collections[match.params.collectionId];
-          const { title, items } = collection;
-          return(
-            <div className='collection-page'>
-              <h2 className='title'>{title}</h2>
-              <div className='items'>
-                {
-                  items.map(item => (
-                    <CollectionItem key={item.id} item={item} />
-                  ))
-                }
-              </div>
-            </div>
-          );
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))
         }
-      }
-    </CollectionsContext.Consumer>
+      </div>
+    </div>
   );
 };
 
