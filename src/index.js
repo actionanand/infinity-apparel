fa04883c561d72c555eb2ec8b88e6bf6607ce783
Anand Raja <meetanandns@gmail.com>
2021-08-06 15:07:56 +0000

    complete removal of redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom"
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 
 import App from './App';
-import { store, persistor } from './redux/store';
 import CartProvider from './providers/cart/cart.provider';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <CartProvider>
-        <BrowserRouter>
-          <PersistGate persistor={persistor}>
-            <App />
-          </PersistGate>
-        </BrowserRouter>
-      </CartProvider>
-    </Provider>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
