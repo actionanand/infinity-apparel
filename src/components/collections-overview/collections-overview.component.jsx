c714aedbffeeed84e4df9fb560104958e11a0ef0
Anand Raja <meetanandns@gmail.com>
2021-08-08 07:51:04 +0000

    adding graphQL to shop componnet

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import './collections-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview = ({ collections }) => (
   <div className='collections-overview'>
@@ -17,8 +14,5 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
